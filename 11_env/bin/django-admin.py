2d45af07f2571ce60ef8ac5fcc74d50387b3f3b1
wumingming <694021065@qq.com>
2017-05-29 03:14:37 +0800

    【项目学习实践】Web应用程序开发 1、安装Django环境     a.安装virtualenv环境     b.使用virtualenv创建一个虚拟环境     c.激活虚拟环境     d.安装Django     e.在Django中创建项目     f.创建数据哭 2、在Django中创建应用程序     a.定义模型     b.激活模型 3、管理Django网站     a.创建超级用户     b.向管理网站注册模型

---
+++ 11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wmm/studydata/python_pro_web_application/11_env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ learning_logs/admin.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.contrib import admin
+from learning_logs.models import Topic
+
+# Register your models here.
+admin.site.register(Topic)
